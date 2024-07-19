77315198751d2495b77d20373a3d137040a6c8b3
GabrielAzamfiri <167236687+GabrielAzamfiri@users.noreply.github.com>
2024-07-19 18:17:50 +0200

    piccole modifche

--- src/components/MainInfo-AsideBar/Profili.jsx
+++ src/components/MainInfo-AsideBar/Profili.jsx
@@ -11,7 +11,7 @@ const Profili = ({ epicProfile }) => {
         <img src={epicProfile.image} alt="profile image" className="rounded-circle  border-white position-absolute" width={50} height={50} />
       </Col>
       <Col className="p-0">
-        <a className="m-0 fs-6 d-block text-dark fw-bold link-underline link-underline-opacity-0 link-underline-opacity-75-hover" onClick={() => dispatch(selectProfileAction(epicProfile))}>
+        <a className=" fs-6 asideLink d-block text-dark fw-bold link-underline link-underline-opacity-0 link-underline-opacity-75-hover" onClick={() => dispatch(selectProfileAction(epicProfile))}>
           {epicProfile.name} {epicProfile.surname}
         </a>
         <span className=" opacity-75 ">{epicProfile.title}</span>
